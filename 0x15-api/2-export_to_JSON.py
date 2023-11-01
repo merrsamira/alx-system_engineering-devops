50017ea5fe2ed99bd963f0c5cb5bc2fee34792a4
merrsamira <125443409+merrsamira@users.noreply.github.com>
2023-11-01 08:50:06 +0100

    ms

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+""" Python script to export data in the JSON format
+"""
+
+
+from requests import get
+from sys import argv
 import json
-import requests
-import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+
+    for j in data2:
+        if j['id'] == int(argv[1]):
+            u_name = j['username']
+            id_no = j['id']
+
+    row = []
+
+    for j in data:
+
+        new_dict = {}
+
+        if j['userId'] == int(argv[1]):
+            new_dict['username'] = u_name
+            new_dict['task'] = j['title']
+            new_dict['completed'] = j['completed']
+            row.append(new_dict)
+
+    final_dict = {}
+    final_dict[id_no] = row
+    json_obj = json.dumps(final_dict)
+
+    with open(argv[1] + ".json", "w") as f:
+        f.write(json_obj)
